5cd77667e1655f1dc44a9b75e411ad2af3b3eb6e
Alexander Daichendt <a.daichendt@tv1.eu>
2024-08-01 17:31:39 +0200

    chore: include dist

---
+++ dist/speech.d.ts
@@ -0,0 +1,56 @@
+export type TranscriptEvent = {
+    kind: "transcript";
+    payload: {
+        id: string;
+        text: string;
+        latency: number;
+        speaker: string;
+        created_at: number;
+        from_ms: number;
+        to_ms: number;
+    };
+};
+export type PartialEvent = {
+    kind: "partial";
+    payload: {
+        text: string;
+        latency: number;
+    };
+};
+export type TranslationEvent = {
+    kind: "translation";
+    payload: {
+        id: string;
+        text: string;
+        original: string;
+        latency: number;
+        speaker: string;
+        created_at: number;
+        from_ms: number;
+        to_ms: number;
+    };
+};
+export type VoiceoverEvent = {
+    kind: "voiceover";
+    payload: {
+        id: string;
+        text: string;
+        original: string;
+        latency: number;
+        speaker: string;
+        created_at: number;
+        playback_uri: string;
+        from_ms: number;
+        to_ms: number;
+    };
+};
+export type EndOfStreamEvent = {
+    kind: "end_of_stream";
+    payload: {
+        reason: string;
+    };
+};
+export interface WebSocketMessage {
+    events: Array<TranscriptEvent | PartialEvent | TranslationEvent | VoiceoverEvent | EndOfStreamEvent>;
+}
+//# sourceMappingURL=speech.d.ts.map
